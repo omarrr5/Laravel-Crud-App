d80f2e40200f2ae9834093f595f9eda45d85e4ce
omar tarik yehya <khalaf.omar20@gmail.com>
2023-07-22 23:42:38 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <div style="border: 3px solid black;">
+            <h2>Register</h2>
+            <form action="/register" method="post">
+                <input type="text" placeholder="name">
+                <input type="text" placeholder="email">
+                <input type="password" placeholder="password">
+                <button>Register</button>
+            </form>
+        </div>
+    </body>
+</html>
